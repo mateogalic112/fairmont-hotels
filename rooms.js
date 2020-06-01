5541435b5ffed68f575bc7cb0113f0aa56fa4e94
mateogalic112 <mateogalic112@gmail.com>
2020-06-01 22:36:47 +0200

    almost ready

--- rooms.js
+++ rooms.js
@@ -9,7 +9,8 @@ function Room(
   extras,
   image,
   info,
-  specialOffer
+  specialOffer,
+  featured
 ) {
   (this.name = name),
     (this.person = person),
@@ -20,7 +21,8 @@ function Room(
     (this.extras = extras),
     (this.image = image),
     (this.info = info),
-    (this.specialOffer = specialOffer);
+    (this.specialOffer = specialOffer),
+    (this.featured = featured);
 }
 
 // Room Array
@@ -35,6 +37,7 @@ const rooms = [
     ["Free Wi-Fi"],
     "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     "The standard single room at Hotel Waldinger is a great choice for any guest! Whether you are in Osijek for business or tourism, our standard single rooms provide complete comfort during your stay.",
+    false,
     false
   ),
   new Room(
@@ -47,6 +50,7 @@ const rooms = [
     ["Free Wi-Fi", "Large Balcony"],
     "https://images.pexels.com/photos/271619/pexels-photo-271619.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
     "Bed sizes are 120 x 210 cm and all rooms are equipped with work desks. Bathrooms feature showers. Smješten nadomak Splita, hotel Le Méridien Lav poziva Vas da istražite svaki kilometar impresivnog jadranskog arhipelaga i smaragdno čistog mora.",
+    true,
     true
   ),
   new Room(
@@ -59,11 +63,12 @@ const rooms = [
     ["Private Garden", "Large Balcony", "Free Pet Area"],
     "https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     "Smješten nadomak Splita, hotel Le Méridien Lav poziva Vas da istražite svaki kilometar impresivnog jadranskog arhipelaga i smaragdno čistog mora. Le Méridien Lav je idealno polazište za putovanje dalmatinskom obalom jer se nalazi u blizini povijesnih gradova.",
+    true,
     true
   ),
 ];
 
-// Umetanje soba na stranicu
+// Umetanje soba na stranicu Rooms
 function roomState(currentRooms) {
   var roomsList = document.getElementsByClassName("rooms-list")[0];
   currentRooms.map(createRoomArticle);
@@ -76,7 +81,7 @@ function roomState(currentRooms) {
             src="${room.image}"
             width="180"
             height="180"
-            alt="presidental-room"
+            alt="${room.name}"
         />
         <div class="room-details">
             <h6>Detail Info</h6>
@@ -101,15 +106,14 @@ function roomState(currentRooms) {
     roomItem.innerHTML = roomItemContents;
     roomsList.append(roomItem);
   }
-
-  // Prikaz Special Offera
-  function specialOfferDisplay() {
-    return `
-  <div class="special-offer">
-    <h3>Special Offer</h3>
-  </div>
-  `;
-  }
+}
+// Prikaz Special Offera
+function specialOfferDisplay() {
+  return `
+<div class="special-offer">
+  <h3>Special Offer</h3>
+</div>
+`;
 }
 roomState(rooms);
 
@@ -160,40 +164,41 @@ pets.addEventListener("change", searchFilteredRooms);
 var smoke = document.getElementById("smoking");
 smoking.addEventListener("change", searchFilteredRooms);
 
-
 // Rooms filter
 function searchFilteredRooms() {
   // Rooms for modification
   var newRooms = rooms;
   // Room Type
-  var roomType = document.getElementById('type');
+  var roomType = document.getElementById("type");
   var roomTypeSelected = roomType.options[roomType.selectedIndex].text;
   // Number of Guests
-  var guest = document.getElementById('guest');
+  var guest = document.getElementById("guest");
   var guestNumberSelected = guest.options[guest.selectedIndex].text;
   // Price Range
-  var minRoomPrice = document.getElementById('minPrice');
+  var minRoomPrice = document.getElementById("minPrice");
   var minRoomPriceSet = minRoomPrice.value;
-  var maxRoomPrice = document.getElementById('maxPrice');
+  var maxRoomPrice = document.getElementById("maxPrice");
   var maxRoomPriceSet = maxRoomPrice.value;
   // Pets
-  var pets = document.getElementById('pets');
+  var pets = document.getElementById("pets");
   var petsSet = pets.checked;
   // Smoking
-  var smoke = document.getElementById('smoking');
-  var smokeSet = smoke.checked;  
+  var smoke = document.getElementById("smoking");
+  var smokeSet = smoke.checked;
 
   // Clear Rooms before Filter
   clearRooms();
   // Filter rooms
-  newRooms = newRooms.filter(room => ((room.name.split(' ')[0] === roomTypeSelected) || roomTypeSelected === 'All')
-    && (room.person >= guestNumberSelected || guestNumberSelected === '-') 
-    && (room.price >= minRoomPriceSet) 
-    && (room.price <= maxRoomPriceSet) 
-    && ((petsSet) ? room.pets == petsSet : true) 
-    && ((smokeSet) ? room.smoking == smokeSet : true)  
+  newRooms = newRooms.filter(
+    (room) =>
+      (room.name.split(" ")[0] === roomTypeSelected ||
+        roomTypeSelected === "All") &&
+      (room.person >= guestNumberSelected || guestNumberSelected === "-") &&
+      room.price >= minRoomPriceSet &&
+      room.price <= maxRoomPriceSet &&
+      (petsSet ? room.pets == petsSet : true) &&
+      (smokeSet ? room.smoking == smokeSet : true)
   );
   // Make them appear in website
   roomState(newRooms);
 }
-
