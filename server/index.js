7a4a808140746430e6b32e5ac4506c7234d7f2d1
Mateo Galic <mateogalic112@gmail.com>
2021-05-26 15:33:43 +0200

    c.

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ const corsOpts = {
 };
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://fairmont-hotels.vercel.app"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
